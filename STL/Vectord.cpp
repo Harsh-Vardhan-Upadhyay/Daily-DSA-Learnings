9393407ab4f1f2a057e4d53e7270cdf6fcad7803
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-12-10 18:30:19 +0530

    learnt about pushback and popback in vector

---
+++ STL/Vectord.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.pop_back();
+
+
+    for (int i : vec){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+    cout << vec.size() <<endl;
+    cout<< vec.capacity() <<endl;
+    return 0;
+}
