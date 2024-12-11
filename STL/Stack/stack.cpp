f9a22a8e6c943a2b49663958a5da51e2555249af
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-12-12 01:03:10 +0530

    :sparkles: Learnt about stacks in STL

--- STL/Stack/stack.cpp
+++ STL/Stack/stack.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    
+    stack<int>s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    cout<<s.top()<<endl;
+
+
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
     return 0;
 }
