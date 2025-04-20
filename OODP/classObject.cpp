b68ad6ca10b9ea584ec2189ea2155c72d75e7f65
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2025-04-20 13:01:54 +0530

    :zap: objects

--- OODP/classObject.cpp
+++ OODP/classObject.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+/*
+    Definitions:
+
+    - Class: A user-defined data type that serves as a blueprint for creating objects. It contains attributes and methods.
+    - Attributes: These are variables defined in a class that hold the state or properties of an object.
+    - Methods: These are functions defined in a class that represent the behavior or actions that an object can perform.
+    - Object: An instance of a class. It is created using the class blueprint and can access attributes and methods defined in the class.
+*/
+
 // This is a class named Human. A class is like a blueprint for creating objects.
 // It can have attributes (data) and methods (functions that act on the data).
 class Human{
